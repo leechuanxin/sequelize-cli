1b1fc0576dbc186f4ac335296c6a53559c240c05
Lee Chuan Xin <chuanxin@gametize.com>
2021-09-10 06:43:09 +0800

    feat: init models.

---
+++ migrations/20210909222357-create-trips-attractions-tables.js
@@ -0,0 +1,72 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    // "trips" table needs to be created first because "attractions" references "trips".
+    await queryInterface.createTable('trips', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      // created_at and updated_at are required
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    await queryInterface.createTable('attractions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      trip_id: {
+        type: Sequelize.INTEGER,
+        // This links the category_id column to the id column in the categories table
+        references: {
+          model: 'trips',
+          key: 'id',
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  // eslint-disable-next-line no-unused-vars
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    // Attractions table needs to be dropped first because Attractions references Trips
+    await queryInterface.dropTable('attractions');
+    await queryInterface.dropTable('trips');
+  },
+};
--- models/index.mjs
+++ models/index.mjs
@@ -2,6 +2,8 @@ import { Sequelize } from 'sequelize';
 import allConfig from '../config/config.js';
 
 // import model functions here
+import initAttractionModel from './attraction.mjs';
+import initTripModel from './trip.mjs';
 
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
@@ -10,6 +12,13 @@ const db = {};
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 // add your model definitions to db here
+db.Attraction = initAttractionModel(sequelize, Sequelize.DataTypes);
+db.Trip = initTripModel(sequelize, Sequelize.DataTypes);
+
+// The following 2 lines enable Sequelize to recognise the 1-M relationship
+// between Item and Category models, providing the mixin association methods.
+db.Attraction.belongsTo(db.Trip);
+db.Trip.hasMany(db.Attraction);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
