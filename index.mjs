c459225ce4c9a6a80705ec494c5157fe26704f03
Lee Chuan Xin <chuanxin@gametize.com>
2021-09-10 07:35:04 +0800

    feat: create attraction for trip.

--- index.mjs
+++ index.mjs
@@ -6,12 +6,12 @@ const createTrip = async () => {
   try {
     const existingTrip = await db.Trip.findOne({
       where: {
-        name: [process.argv[3]],
+        name: process.argv[3],
       },
     });
 
     if (existingTrip) {
-      throw new Error('This trip already exists!');
+      throw new Error(`The trip ${existingTrip} already exists!`);
     }
 
     const { dataValues } = await db.Trip.create({
@@ -23,13 +23,61 @@ const createTrip = async () => {
   }
 };
 
-if (command === 'create') {
-  if (process.argv[3]) {
-    createTrip();
-  } else {
-    console.error('Please create a new trip with the command:');
-    console.error('node index.mjs create <tripName>');
+const addAttraction = async () => {
+  try {
+    const existingTrip = await db.Trip.findOne({
+      where: {
+        name: process.argv[3],
+      },
+    });
+
+    if (!existingTrip) {
+      throw new Error(`The trip "${process.argv[3]}" does not exist!`);
+    }
+
+    const { dataValues } = await db.Attraction.create({
+      name: process.argv[4],
+      tripId: existingTrip.id,
+    });
+    console.log(`A new attraction "${dataValues.name}" has been created, and assigned to the trip "${existingTrip.name}"!`);
+  } catch (error) {
+    console.error(error);
   }
+};
+
+const logCommands = () => {
+  console.error('#########################################################');
+  console.error('To create a trip:');
+  console.error('node index.mjs create <tripName>');
+  console.error('---------------------------------------------------------');
+  console.error('To add an attraction:');
+  console.error('node index.mjs add-attrac <tripName> <attractionName>');
+  console.error('---------------------------------------------------------');
+  console.error('To get an itinerary for a trip:');
+  console.error('node index.mjs trip <tripName>');
+  console.error('#########################################################');
+};
+
+switch (command) {
+  case 'create':
+    if (process.argv[3]) {
+      createTrip();
+    } else {
+      console.error('Please create a new trip with the command:');
+      console.error('node index.mjs create <tripName>');
+    }
+    break;
+  case 'add-attrac':
+    if (process.argv[3] && process.argv[4]) {
+      addAttraction();
+    } else {
+      console.error('Please add an attraction with the command:');
+      console.error('node index.mjs add-attrac <tripName> <attractionName>');
+    }
+    break;
+  default:
+    logCommands();
+    break;
 }
 
 // db.<MODEL_NAME_CAPITALIZED_SINGULAR>
