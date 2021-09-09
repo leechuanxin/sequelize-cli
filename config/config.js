85bab6653ea1acf0a51ba3de9f329c5a84791c44
Lee Chuan Xin <chuanxin@gametize.com>
2021-09-10 06:23:12 +0800

    chore: create db.

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'chuanxin',
     password: null,
-    database: '<YOUR_DATABASE_NAME>',
+    database: 'itinerary_development',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
